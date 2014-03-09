c711519a69c2453afb85ddd1a6d0b34bdb357df9
Jouni <jjvaris@gmail.com>
2014-03-10 01:04:25 +0200

    herokufix

--- config/refreshHighscores.js
+++ config/refreshHighscores.js
@@ -14,7 +14,7 @@ function startKeepAlive() {
       res.on('data', function(chunk) {
         try {
           // optional logging... disable after it's working
-          console.log("HEROKU RESPONSE: " + chunk);
+          //console.log("HEROKU RESPONSE: " + chunk);
         } catch (err) {
           console.log("error1 "+err.message);
         }
