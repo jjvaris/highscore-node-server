73ee2d820dbae92cc66fd0d32603e29eb6f08802
jjvaris@gmail.com <jjvaris@Jounis-Air.mshome.net>
2015-10-09 16:49:41 +0300

    mongoose update

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,12 @@
 module.exports = function(app){
 
 	//home route
-	var home = require('../app/controllers/home');
+	//
+  var home = require('../app/controllers/home');
   var highscores = require('../app/controllers/highscores');
   var logger = require('../app/controllers/logger');
-	app.get('/', home.index);
+
+  app.get('/', home.index);
 
   //app.get('/api/highscores', highscores.get);
   app.post('/api/highscores', highscores.addHighscore);
